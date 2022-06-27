d4c17311165ea1740f5dbdf8307da970254643da
VictorBem <b3m.v@yandex.ru>
2022-06-28 00:06:26 +0300

    feat: добавить класс Person

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Object anyObject = new Person("Михаил");
+        System.out.println(anyObject);
+    }
+}
